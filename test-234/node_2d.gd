44a7d2dd0c90afac1eb63ba2f832fe2a42dcdcda
Philip Dodgion <96752794+SkellyBoney@users.noreply.github.com>
2025-09-11 19:09:53 -0400

    Added food string
    called the food variable in the ready function

--- test-234/node_2d.gd
+++ test-234/node_2d.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
 var hello = "world"
+var food = "Burger"
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print(food)
 	print(hello)
 	pass # Replace with function body.
 
