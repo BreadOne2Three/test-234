08b16c5907a482e171b10cdad10b18be80e5e1b0
BreadOne2Three <143757607+BreadOne2Three@users.noreply.github.com>
2025-09-11 19:12:47 -0400

    merge conflict resolve?

--- test-234/node_2d.gd
+++ test-234/node_2d.gd
@@ -1,7 +1,13 @@
 extends Node2D
 
+<<<<<<< Updated upstream
 var hello = "world"
 var food = "Burger"
+=======
+var hello : String = "world"
+
+var health : int = 100
+>>>>>>> Stashed changes
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print(food)
