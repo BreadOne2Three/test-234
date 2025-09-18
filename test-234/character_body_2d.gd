18e7c4144135c578fb134847ed5c802cd1622687
Philip Dodgion <96752794+SkellyBoney@users.noreply.github.com>
2025-09-18 09:50:39 -0400

    Code for the club demo

---
+++ test-234/character_body_2d.gd
@@ -0,0 +1,66 @@
+extends CharacterBody2D
+
+
+const SPEED = 500.0
+const JUMP_VELOCITY = -600.0
+const FALL_VELOCITY = 250
+var wallStick := false
+var leftRight
+var wallTimer := 3.0
+
+
+func _physics_process(delta: float) -> void:
+	if wallTimer > 0:
+		wallTimer -= delta
+	
+	
+	var direction := Input.get_axis("ui_left", "ui_right")
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	elif Input.is_action_just_pressed("ui_accept") and wallStick:
+		print("test")
+		velocity.y = JUMP_VELOCITY
+		if leftRight:
+			velocity.x = 250
+		else:
+			velocity.x = -250
+		wallStick = false
+		
+	if not is_on_floor() and velocity.y != 0:
+		$Sprite2D.texture = preload("res://Assets/CharacterJump.tres")
+		
+	if is_on_floor() and velocity.x == 0:
+		$Sprite2D.texture = preload("res://Assets/CharacterIdle.tres")
+	
+	if Input.is_action_just_pressed("ui_down"):
+		velocity.y = FALL_VELOCITY
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	
+	if direction and not wallStick:
+		velocity.x = direction * SPEED
+	elif not wallStick:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if velocity.x != 0 and is_on_floor():
+		$Sprite2D.texture = preload("res://Assets/CharacterMove.tres")
+		
+	if velocity.x < 0: # Moving left
+		$Sprite2D.flip_h = false
+		leftRight = true
+	elif velocity.x > 0: # Moving right
+		$Sprite2D.flip_h = true
+		leftRight = false
+		
+	if is_on_wall() and not is_on_floor() and not Input.is_action_just_pressed("ui_accept"):
+		wallStick = true
+		velocity.y = 0
+		$Sprite2D.texture = preload("res://Assets/CharacterClimb.tres")
+	
+
+	move_and_slide()
--- test-234/node_2d.gd
+++ test-234/node_2d.gd
@@ -1,17 +1,8 @@
 extends Node2D
 
-<<<<<<< Updated upstream
-var hello = "world"
-var food = "Burger"
-=======
-var hello : String = "world"
-
-var health : int = 100
->>>>>>> Stashed changes
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(food)
-	print(hello)
+	
 	pass # Replace with function body.
 
 
