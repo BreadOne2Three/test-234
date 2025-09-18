41c29378e0db8d83a602de886842ba4b59046c8c
Philip Dodgion <96752794+SkellyBoney@users.noreply.github.com>
2025-09-18 17:46:18 -0400

    Finished Demo

--- test-234/character_body_2d.gd
+++ test-234/character_body_2d.gd
@@ -6,13 +6,12 @@ const JUMP_VELOCITY = -600.0
 const FALL_VELOCITY = 250
 var wallStick := false
 var leftRight
-var wallTimer := 3.0
+var wallTimer := 0.5
+var wallTimerStart := false
+var canWallStick := true
 
 
 func _physics_process(delta: float) -> void:
-	if wallTimer > 0:
-		wallTimer -= delta
-	
 	
 	var direction := Input.get_axis("ui_left", "ui_right")
 	# Add the gravity.
@@ -23,13 +22,21 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	elif Input.is_action_just_pressed("ui_accept") and wallStick:
-		print("test")
 		velocity.y = JUMP_VELOCITY
 		if leftRight:
 			velocity.x = 250
 		else:
 			velocity.x = -250
 		wallStick = false
+		wallTimerStart = true
+		
+	if wallTimer > 0 and wallTimerStart:
+		wallTimer -= delta
+		canWallStick = false
+	elif wallTimerStart:
+		canWallStick = true
+		wallTimerStart = false
+		wallTimer = 0.5
 		
 	if not is_on_floor() and velocity.y != 0:
 		$Sprite2D.texture = preload("res://Assets/CharacterJump.tres")
@@ -44,7 +51,7 @@ func _physics_process(delta: float) -> void:
 	
 	if direction and not wallStick:
 		velocity.x = direction * SPEED
-	elif not wallStick:
+	elif not wallStick and canWallStick:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
 	if velocity.x != 0 and is_on_floor():
@@ -57,10 +64,11 @@ func _physics_process(delta: float) -> void:
 		$Sprite2D.flip_h = true
 		leftRight = false
 		
-	if is_on_wall() and not is_on_floor() and not Input.is_action_just_pressed("ui_accept"):
+	if is_on_wall() and not is_on_floor() and not Input.is_action_just_pressed("ui_accept") and canWallStick:
 		wallStick = true
 		velocity.y = 0
 		$Sprite2D.texture = preload("res://Assets/CharacterClimb.tres")
+		
 	
 
 	move_and_slide()
